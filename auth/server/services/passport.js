f92ffaae6ce5ddb4268f3f6351249b266fc33617
zippi-wzp <truebatmandc@gmail.com>
2017-07-03 22:28:15 +0300

    added passport jwt strategy.

--- auth/server/services/passport.js
+++ auth/server/services/passport.js
@@ -4,4 +4,23 @@ const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
 
+const JwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(paylaod.sub, function(err, user) {
+    if (err) {
+      return done(err, false);
+    }
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+
+  });
+});
+
+passport.use(JwtLogin);
