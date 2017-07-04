8018f61319f1f74ce0a98e9aec85e2790c30891f
zippi-wzp <truebatmandc@gmail.com>
2017-07-04 15:00:20 +0300

    finished 'server authentication' module.

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -4,10 +4,13 @@ const config = require('../config');
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
-  return jwt.encode({
-    sub: user.id,
-    iat: timestamp
-  }, config.secret);
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  // User has already had their email and password auth'd
+  // We just need to give them a token
+  res.send({ token: tokenForUser(req.user) });
 }
 
 exports.signup = function(req, res, next) {
@@ -15,28 +18,29 @@ exports.signup = function(req, res, next) {
   const password = req.body.password;
 
   if (!email || !password) {
-    return res.status(422).send({error: 'You must provide email and password!'});
+    return res.status(422).send({ error: 'You must provide email and password'});
   }
 
-  User.findOne({email: email}, function(err, existingUser) {
-    if (err) {
-      next(err);
-    }
+  // See if a user with the given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
+
+    // If a user with email does exist, return an error
     if (existingUser) {
-      return res.status(422).send({error: "Email is in use"});
+      return res.status(422).send({ error: 'Email is in use' });
     }
 
+    // If a user with email does NOT exist, create and save user record
     const user = new User({
       email: email,
       password: password
     });
 
     user.save(function(err) {
-      if (err) {
-        return next(err);
-      }
+      if (err) { return next(err); }
 
-      res.json({token: tokenForUser(user)});
+      // Repond to request indicating the user was created
+      res.json({ token: tokenForUser(user) });
     });
   });
-};
+}
--- auth/server/services/passport.js
+++ auth/server/services/passport.js
@@ -3,6 +3,24 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+const localOptions = {usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  User.findOne({email: email}, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) {return done(err);}
+      if (!isMatch) {return done(null, false);}
+      return done(null, user);
+    });
+  });
+});
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
@@ -10,7 +28,7 @@ const jwtOptions = {
 };
 
 const JwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
-  User.findById(paylaod.sub, function(err, user) {
+  User.findById(payload.sub, function(err, user) {
     if (err) {
       return done(err, false);
     }
@@ -24,3 +42,4 @@ const JwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 passport.use(JwtLogin);
+passport.use(localLogin);
